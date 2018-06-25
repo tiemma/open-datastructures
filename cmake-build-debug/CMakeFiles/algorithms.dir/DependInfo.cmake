e9b3bb91c69614342afd05ac0eee78dddd0249c1
blank <bakareemmanuel98@outlook.com>
2018-06-25 13:01:03 +0100

    Debug configurations

---
+++ cmake-build-debug/CMakeFiles/algorithms.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/blank/CLionProjects/algorithms/List/List.cpp" "/home/blank/CLionProjects/algorithms/cmake-build-debug/CMakeFiles/algorithms.dir/List/List.cpp.o"
+  "/home/blank/CLionProjects/algorithms/Queue/PriorityQueue.cpp" "/home/blank/CLionProjects/algorithms/cmake-build-debug/CMakeFiles/algorithms.dir/Queue/PriorityQueue.cpp.o"
+  "/home/blank/CLionProjects/algorithms/Queue/Queue.cpp" "/home/blank/CLionProjects/algorithms/cmake-build-debug/CMakeFiles/algorithms.dir/Queue/Queue.cpp.o"
+  "/home/blank/CLionProjects/algorithms/Solutions/Chapter 1/File.cpp" "/home/blank/CLionProjects/algorithms/cmake-build-debug/CMakeFiles/algorithms.dir/Solutions/Chapter_1/File.cpp.o"
+  "/home/blank/CLionProjects/algorithms/Stack/Stack.cpp" "/home/blank/CLionProjects/algorithms/cmake-build-debug/CMakeFiles/algorithms.dir/Stack/Stack.cpp.o"
+  "/home/blank/CLionProjects/algorithms/UnorderedSet/USet.cpp" "/home/blank/CLionProjects/algorithms/cmake-build-debug/CMakeFiles/algorithms.dir/UnorderedSet/USet.cpp.o"
+  "/home/blank/CLionProjects/algorithms/main.cpp" "/home/blank/CLionProjects/algorithms/cmake-build-debug/CMakeFiles/algorithms.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/algorithms.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/algorithms.dir/main.cpp.o"
+  "CMakeFiles/algorithms.dir/Queue/Queue.cpp.o"
+  "CMakeFiles/algorithms.dir/Queue/PriorityQueue.cpp.o"
+  "CMakeFiles/algorithms.dir/Stack/Stack.cpp.o"
+  "CMakeFiles/algorithms.dir/List/List.cpp.o"
+  "CMakeFiles/algorithms.dir/UnorderedSet/USet.cpp.o"
+  "CMakeFiles/algorithms.dir/Solutions/Chapter_1/File.cpp.o"
+  "algorithms.pdb"
+  "algorithms"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/algorithms.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
